b3c83f22ebadd215ac91e510cde7450a6e278bb5
ClzSkywalker <44250611+ClzSkywalker@users.noreply.github.com>
2023-10-24 23:06:02 +0800

    Update init.lua

--- lua/bloc/init.lua
+++ lua/bloc/init.lua
@@ -10,7 +10,16 @@ function M.create_cubit(name, parent_folder_relative_path)
 	local path = parent_folder_relative_path
 	local cubit_folder_path = path .. name .. "_cubit";
 	os.execute("mkdir " .. cubit_folder_path)
-	cubit_folder_path = cubit_folder_path .. "\\"
+	
+	local platform = os.getenv("OS")
+	local newline
+	if platform == "Windows_NT" then
+    		newline = "\\"
+	else
+    		newline = "/"
+	end
+	
+	cubit_folder_path = cubit_folder_path .. newline
 	local cubit_path = cubit_folder_path .. name .. "_cubit.dart";
 	local state_path = cubit_folder_path .. name .. "_state.dart";
 
@@ -25,7 +34,16 @@ function M.create_bloc(name, parent_folder_relative_path)
 	local path = parent_folder_relative_path
 	local bloc_folder_path = path .. name .. "_bloc";
 	os.execute("mkdir " .. bloc_folder_path)
-	bloc_folder_path = bloc_folder_path .. "\\"
+
+	local platform = os.getenv("OS")
+	local newline
+	if platform == "Windows_NT" then
+    		newline = "\\"
+	else
+    		newline = "/"
+	end
+	
+	bloc_folder_path = bloc_folder_path .. newline
 	local bloc_path = bloc_folder_path .. name .. "_bloc.dart";
 	local state_path = bloc_folder_path .. name .. "_state.dart";
 	local events_path = bloc_folder_path .. name .. "_events.dart";
