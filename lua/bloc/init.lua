50aff132918fc60fae4e2de3baf9dbba97a3909a
chenlongzu <1045683477@qq.com>
2023-10-25 10:20:25 +0800

    fix(path):

--- lua/bloc/init.lua
+++ lua/bloc/init.lua
@@ -11,13 +11,14 @@ function M.create_cubit(name, parent_folder_relative_path)
 	local cubit_folder_path = path .. name .. "_cubit";
 	os.execute("mkdir " .. cubit_folder_path)
 	
-	local platform = os.getenv("OS")
-	local newline
-	if platform == "Windows_NT" then
-    		newline = "\\"
-	else
-    		newline = "/"
-	end
+	-- local platform = os.getenv("OS")
+	-- local newline
+	-- if platform == "Windows_NT" then
+    -- 		newline = "\\"
+	-- else
+    -- 		newline = "/"
+	-- end
+	local newline= "/"
 	
 	cubit_folder_path = cubit_folder_path .. newline
 	local cubit_path = cubit_folder_path .. name .. "_cubit.dart";
@@ -35,13 +36,14 @@ function M.create_bloc(name, parent_folder_relative_path)
 	local bloc_folder_path = path .. name .. "_bloc";
 	os.execute("mkdir " .. bloc_folder_path)
 
-	local platform = os.getenv("OS")
-	local newline
-	if platform == "Windows_NT" then
-    		newline = "\\"
-	else
-    		newline = "/"
-	end
+	-- local platform = os.getenv("OS")
+	-- local newline
+	-- if platform == "Windows_NT" then
+    -- 		newline = "\\"
+	-- else
+    -- 		newline = "/"
+	-- end
+	local newline= "/"
 	
 	bloc_folder_path = bloc_folder_path .. newline
 	local bloc_path = bloc_folder_path .. name .. "_bloc.dart";
