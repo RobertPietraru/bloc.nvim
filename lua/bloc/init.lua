5b7a93da1acc60c55dd9351f28f28da6b2b545fb
RobertPietraru <rob_piet@yahoo.com>
2023-10-27 19:20:02 +0300

    possible fix

--- lua/bloc/init.lua
+++ lua/bloc/init.lua
@@ -9,7 +9,15 @@ function M.create_cubit(name, parent_folder_relative_path)
 	local cubit_templates = templates.cubit;
 	local path = parent_folder_relative_path
 	local cubit_folder_path = path .. name .. "_cubit";
-	
+	local platform = os.getenv("OS")
+	local newline
+	if platform == "Windows_NT" then
+		newline = "\\"
+	else
+		newline = "/"
+	end
+
+	cubit_folder_path = cubit_folder_path .. newline
 	cubit_folder_path = cubit_folder_path .. "/"
 	local cubit_path = cubit_folder_path .. name .. "_cubit.dart";
 	local state_path = cubit_folder_path .. name .. "_state.dart";
@@ -26,7 +34,15 @@ function M.create_bloc(name, parent_folder_relative_path)
 	local bloc_folder_path = path .. name .. "_bloc";
 	os.execute("mkdir " .. bloc_folder_path)
 
-	bloc_folder_path = bloc_folder_path .. "/"
+	local platform = os.getenv("OS")
+	local newline
+	if platform == "Windows_NT" then
+		newline = "\\"
+	else
+		newline = "/"
+	end
+
+	bloc_folder_path = bloc_folder_path .. newline
 	local bloc_path = bloc_folder_path .. name .. "_bloc.dart";
 	local state_path = bloc_folder_path .. name .. "_state.dart";
 	local events_path = bloc_folder_path .. name .. "_events.dart";
@@ -221,6 +237,5 @@ function M.setup()
 
 	null_ls.setup()
 end
-
 
 return M
